5d166458e39c32c017e4ed0a851ffd1e0fc8a129
karina-90 <80434029+karina-90@users.noreply.github.com>
2021-07-22 16:56:24 -0700

    first commit

---
+++ src/main/java/Jenkins.java
@@ -0,0 +1,13 @@
+public class Jenkins {
+    public static void main (String[] args) throws InterruptedException {
+       for(int i =0; i<5; i++){
+           System.out.println("Karina's practice code");
+           Thread.sleep(2000);
+
+       }
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Karina's practice code 2");
+            Thread.sleep(2000);
+        }
+    }
+}
